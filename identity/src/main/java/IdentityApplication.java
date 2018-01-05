39b44894be751373183a5fc56c60aa1041fee818
BanpilinAF <zhangliwei@archforce.com.cn>
2018-01-05 15:02:28 +0800

    remove .idea folder,add file out of control

---
+++ identity/src/main/java/IdentityApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: Banpil {@link zlw459@126.com}
+ * Date: 2018/1/2 17:33
+ */
+@SpringBootApplication
+public class IdentityApplication {
+
+}
